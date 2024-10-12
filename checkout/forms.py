fb1b4b8087abc05ebdf002a89a987a132f7380ab
KevinFlanagan <flanagank777@gmail.com>
2024-10-12 11:48:57 +0100

    Fix checkout app python code for validation.

--- checkout/forms.py
+++ checkout/forms.py
@@ -27,7 +27,7 @@ class OrderForm(forms.ModelForm):
             'county': 'County, State or Locality',
         }
 
-        self.fields['full_name'].widget.attrs['autofocus'] = True # cursor placed in full_name field
+        self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
             if field != 'country':
                 if self.fields[field].required:
